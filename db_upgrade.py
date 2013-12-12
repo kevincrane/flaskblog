8c81820ba632cf33f4f2437eb97f5f1f02435123
Kevin Crane <kevincrane@gmail.com>
2013-12-12 18:17:12 -0500

    Finished part IV (added a SQLite DB)

---
+++ db_upgrade.py
@@ -0,0 +1,9 @@
+#!/home/kevin/.virtualenvs/flaskblog/bin/python
+#TODO: cleaner way to make above path?
+
+from migrate.versioning import api
+from config import SQLALCHEMY_DATABASE_URI
+from config import SQLALCHEMY_MIGRATE_REPO
+
+api.upgrade(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO)
+print 'Current database version: ' + str(api.db_version(SQLALCHEMY_DATABASE_URI, SQLALCHEMY_MIGRATE_REPO))
